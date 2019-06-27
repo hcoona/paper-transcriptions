b0cffb74c6e5cf7ff729efb6945c468a24c50fe1
Shuai Zhang <zhangshuai.ds@bytedance.com>
2019-06-27 16:13:53 +0800

    Update

--- src/main/python/build_tool/build_tool.py
+++ src/main/python/build_tool/build_tool.py
@@ -2,5 +2,32 @@
 Entry Point of the customized build tool.
 """
 
+import os
+import sys
+
+EXCLUDED_DIRECTORIES = set([
+    ".git",
+    "archive",
+    "common",
+    "src",
+])
+
+def __main(path):
+    directories = list(
+        filter(
+            os.path.isdir,
+            map(
+                lambda p: os.path.join(path, p),
+                filter(
+                    lambda p: not p.startswith('bazel-'),
+                    filter(
+                        lambda p: p not in EXCLUDED_DIRECTORIES,
+                        os.listdir(path))))))
+    print(directories)
+
 if __name__ == '__main__':
-    print('Hello World!')
+    if len(sys.argv) == 1:
+        path = os.getcwd()
+    else:
+        path = sys.argv[1]
+    __main(path)
