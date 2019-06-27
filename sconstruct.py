be99cd7b9fe59550da9719a62ab9a9f1a5b0ebaf
Shuai Zhang <zhangshuai.ds@bytedance.com>
2019-06-27 16:33:37 +0800

    Add DOI:10.1561/1900000002

--- sconstruct.py
+++ sconstruct.py
@@ -15,11 +15,21 @@ ENV = Environment(
     ENV=os.environ,
     BUILDERS={'latexmk': LATEXMK})
 
-DIRS = filter(os.path.isdir, os.listdir())
+DIRS = filter(
+    lambda p: (not p in EXCLUDED_DIR) and (not p.startswith('bazel-')),
+    filter(
+        os.path.isdir,
+        os.listdir()))
 for d in DIRS:
-    if not d in EXCLUDED_DIR:
-        t = ENV.latexmk('archive/' + d + '.pdf', d + '/main.tex')
-        ENV.Depends(t, Glob('common/*.tex'))
-        ENV.Depends(t, Glob(os.path.join(d, '*.png')))
+    others = filter(
+        lambda p: p.endswith('.tex') and p != 'main.tex',
+        os.listdir(d))
+    for f in others:
+        tf = ENV.latexmk(os.path.join(d, f[:-4] + '.pdf'), os.path.join(d, f))
+        ENV.Depends(tf, Glob('common/*.tex'))
+    t = ENV.latexmk('archive/' + d + '.pdf', d + '/main.tex')
+    ENV.Depends(t, Glob('common/*.tex'))
+    ENV.Depends(t, Glob(os.path.join(d, '*.png')))
+    ENV.Depends(t, Glob(os.path.join(d, '*.pdf')))
 
 # TODO: generate README.adoc Index automatically
